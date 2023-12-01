c0250824872d2bfa608d009d54e7d1a8309e2b0a
Alkimoru <135190551+Alkimoru@users.noreply.github.com>
2023-12-02 02:43:10 +0300

    Добавил второе задание

---
+++ Ex_2/Program.cs
@@ -0,0 +1,40 @@
+﻿void Generation(int[] collection)
+{
+    int index_generation = 0;
+    while (index_generation < collection.Length)
+    {
+        collection[index_generation] = new Random().Next(10, 100);
+        index_generation++;
+    }
+}
+void PrintArray(int[] print)
+{
+    int index_printarray = 0;
+    Console.Write("{");
+    while (index_printarray < print.Length)
+    {
+
+        Console.Write(print[index_printarray]);
+        Console.Write(", ");
+
+        index_printarray++;
+    }
+    Console.Write("}");
+    Console.WriteLine(" ");
+}
+int[] array = new int[10];
+Generation(array);
+PrintArray(array);
+int index = 0;
+int result = 0;
+while (index < array.Length)
+{
+    if (array[index] % 2 == 0)
+    {
+        result++;
+    }
+
+    index++;
+}
+Console.Write("Колличество чётных элементов в массиве равно: ");
+Console.Write(result);
