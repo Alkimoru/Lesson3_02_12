12d1392b252dcf02bc1b6ee0c69c77a5f14135ca
Alkimoru <135190551+Alkimoru@users.noreply.github.com>
2023-12-02 02:35:10 +0300

    Добавил первое задание

---
+++ Ex_1/Program.cs
@@ -0,0 +1,36 @@
+﻿void Generation(int[] collection)
+{
+    int index_generation = 0;
+    while (index_generation < collection.Length)
+    {
+        collection[index_generation] = new Random().Next(10, 100);
+        index_generation++;
+    }
+}
+void PrintArray(int[] print)
+{
+    int position = 0;
+    Console.Write("{");
+    while (position < print.Length)
+    {
+
+        Console.Write(print[position]);
+        Console.Write(", ");
+
+        position++;
+    }
+    Console.Write("}");
+}
+int[] array = new int[10];
+Generation(array);
+PrintArray(array);
+int index = 0;
+int result = 0;
+while (index < array.Length)
+{
+    if (array[index] <= 90 && array[index] >= 20) result++;
+    index++;
+}
+Console.WriteLine(" ");
+Console.Write("Колличество элементов массива лежащих в отрезке [20;90] равно ");
+Console.Write(result);
